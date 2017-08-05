f82e8c7804cbd1087a19105dc5af14b30a920cdd
Xianhui Feng <xianhui.feng100@gmail.com>
2017-08-05 14:41:20 -0700

    Env: Set up react environment, add webpack and dependencies.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+// import { SkiDayCount } from './components/SkiDayCount'
+
+window.React = React
+
+render(
+	<p> Testing react first step</p>,
+	document.getElementById('react-container')
+)
